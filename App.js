f519cadbf5f60a2df11880c6b8299cbb6121434e
fx109138 <1091381572@qq.com>
2018-01-08 00:56:18 +0800

    bar finished

--- App.js
+++ App.js
@@ -4,34 +4,85 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
   Platform,
   StyleSheet,
   Text,
+  Image,
   View
 } from 'react-native';
+import {Navigator} from 'react-native-deprecated-custom-components';
+import Boy from './Boy';
+import TabNavigator from 'react-native-tab-navigator';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
+  'Cmd+D or shake for dev menu',
   android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+  'Shake or press menu button for dev menu',
 });
 
 export default class App extends Component<{}> {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedTab: 'tab_popular',
+    };
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        {/*<TabNavigator>
+          <TabNavigator.Item
+            selected={this.state.selectedTab === 'tab_popular'}
+            selectedTitleStyle={{color: 'red'}}
+            title="最热"
+            renderIcon={() => <Image style={styles.image} source={require('./res/images/ic_popular.png')}/>}
+            renderSelectedIcon={() => <Image style={[styles.image, {tintColor: 'red'}]}
+                                             source={require('./res/images/ic_popular.png')}/>}
+            onPress={() => this.setState({selectedTab: 'tab_popular'})}>
+            <View style={styles.page1}></View>
+          </TabNavigator.Item>
+          <TabNavigator.Item
+            selected={this.state.selectedTab === 'tab_trending'}
+            selectedTitleStyle={{color: 'yellow'}}
+            title="趋势"
+            renderIcon={() => <Image style={styles.image} source={require('./res/images/ic_trending.png')}/>}
+            renderSelectedIcon={() => <Image style={[styles.image, {tintColor: 'yellow'}]}
+                                             source={require('./res/images/ic_trending.png')}/>}
+            onPress={() => this.setState({selectedTab: 'tab_trending'})}>
+            <View style={styles.page2}></View>
+          </TabNavigator.Item>
+          <TabNavigator.Item
+            selected={this.state.selectedTab === 'tab_favorite'}
+            selectedTitleStyle={{color: 'red'}}
+            title="收藏"
+            renderIcon={() => <Image style={styles.image} source={require('./res/images/ic_favorite.png')}/>}
+            renderSelectedIcon={() => <Image style={[styles.image, {tintColor: 'red'}]}
+                                             source={require('./res/images/ic_popular.png')}/>}
+            onPress={() => this.setState({selectedTab: 'tab_favorite'})}>
+            <View style={styles.page1}></View>
+          </TabNavigator.Item>
+          <TabNavigator.Item
+            selected={this.state.selectedTab === 'tab_my'}
+            selectedTitleStyle={{color: 'yellow'}}
+            title="我的"
+            renderIcon={() => <Image style={styles.image} source={require('./res/images/ic_my.png')}/>}
+            renderSelectedIcon={() => <Image style={[styles.image, {tintColor: 'yellow'}]}
+                                             source={require('./res/images/ic_trending.png')}/>}
+            onPress={() => this.setState({selectedTab: 'tab_my'})}>
+            <View style={styles.page2}></View>
+          </TabNavigator.Item>
+        </TabNavigator>*/}
+        <Navigator initialRoute={{
+          component: Boy
+        }} renderScene={(route, navigator) => {
+          let Component = route.component;
+          return <Component navigator={navigator} {...route.params}/>
+        }}></Navigator>
       </View>
     );
   }
@@ -40,18 +91,18 @@ export default class App extends Component<{}> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  page1: {
+    flex: 1,
+    backgroundColor: 'red',
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  page2: {
+    flex: 1,
+    backgroundColor: 'yellow',
   },
+  image: {
+    height: 22,
+    width: 22
+  }
 });
